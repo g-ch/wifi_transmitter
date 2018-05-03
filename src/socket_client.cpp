64f7f0e568b3a9583e6f6653342feb1336f6e749
g-ch <947089399@qq.com>
2018-05-04 04:14:43 +0800

    complete

--- src/socket_client.cpp
+++ src/socket_client.cpp
@@ -10,17 +10,28 @@
 #include <unistd.h>  
 #include <arpa/inet.h>  
 #include <sys/socket.h>
+
+#include <pcl/point_types.h>
+#include <pcl_conversions/pcl_conversions.h>
+
 //#include <cv.h>   
-//#define ADDR "192.168.1.144"   
-#define ADDR "127.0.0.1" //在本机测试用这个地址，如果连接其他电脑需要更换IP  
+#define ADDR "192.168.1.102" 
+//#define ADDR "127.0.0.1" //在本机测试用这个地址，如果连接其他电脑需要更换IP  
 
 #define SERVERPORT 10001  
 #define BYTE_SEND_INTERVAL 1
+#define HALF_RANGE 3.2
+#define RESOLUSTION 0.1
 
 using namespace std;
 using namespace cv;
 int sock_clit;
 struct sockaddr_in serv_addr; 
+
+pcl::PointCloud<pcl::PointXYZ> cloud;
+int point_num, cur_num;
+bool cloud_broken;
+int broken_check;
 
 int send_data(char* data) //255k at most
 {
@@ -41,16 +52,37 @@ int send_data(char* data) //255k at most
 	return 1;
 }
 
+
+void timerCallback(const ros::TimerEvent &e)
+{
+    broken_check ++;
+    if(broken_check > 10000) //Just in case
+        broken_check = 10000;
+    if(broken_check > 20) //2 seconds
+        cloud_broken = true;
+    else 
+        cloud_broken = false;
+}
+
 /* @function main */
 int main( int argc, char **argv )
 {
     ros::init(argc, argv, "socket_client");
     ros::NodeHandle n;
 
+    /* publisher to rviz*/
+    ros::Publisher cloud_pub = n.advertise<sensor_msgs::PointCloud2>("client/cloud", 500);
+    ros::Duration(0.5).sleep();
+
+    /* Watch dog for cloud */
+    broken_check = 0;
+    cloud_broken = false;
+    ros::Timer timer = n.createTimer(ros::Duration(0.1), timerCallback);
+
     /*compress paras init*/
     vector<int> param= vector<int>(2); 
     param[0]=CV_IMWRITE_JPEG_QUALITY; 
-    param[1]=50;//default(95) 0-100 
+    param[1]=40;//default(95) 0-100 
 
     /*socket paras init*/   
     char get_msg[10] = {0};    
@@ -59,6 +91,13 @@ int main( int argc, char **argv )
     sock_clit = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
     struct sockaddr_in c_in; 
     socklen_t len=sizeof(struct sockaddr);
+
+    struct timeval timeout;
+    timeout.tv_sec = 1;//秒
+    timeout.tv_usec = 0;//微秒
+    if (setsockopt(sock_clit, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout)) == -1) {
+        cout<<"setsockopt failed:"<<endl;
+    }
 
     if(sock_clit == -1){ 
 
@@ -77,70 +116,169 @@ int main( int argc, char **argv )
         cout << "connected ...\n" << endl;  //注释3  
     }  
 
-  	Size rec_size = Size(1280,720);
-    Mat rec_img = Mat(rec_size, CV_8UC3);
-  	namedWindow("client");
-
     /*define mat for image*/
+  	Size rec_size = Size(640,480);
+    Mat rec_img = Mat(rec_size, CV_8UC3);
+    Mat HSV_img;
+  	namedWindow("client");
+    namedWindow("target");
     
     cout<<"transmission started"<<endl;
 
     char quit_flag = ' ';
-    unsigned char buf[1024]="";//content buff area 
+
+    unsigned char image_buf[1024]="";//content buff area, image
+    double buf[1024];  // content buff area, cloud
+
+    int trans_type = 1;
 
     while(ros::ok && quit_flag!='q'){
         
 	    
-        char send_buff[8] = "";
-        send_buff[0] = 1;
-        send_buff[1] = 2;
-        send_buff[2] = 3;
-        send_data(send_buff);
+        if(trans_type == 1)
+        {
+            /*request image*/
+            char send_buff[8] = "";
+            send_buff[0] = 1;
+            send_buff[1] = 1;
+            send_buff[2] = 1;
+            send_data(send_buff);
+            cout<<"image request sent"<<endl;
 
-         /*recieve*/
-        int r_len=recvfrom(sock_clit,buf,1024,0,(struct sockaddr*)&c_in,&len);
+             /*recieve*/
+            int r_len=recvfrom(sock_clit,image_buf,1024,0,(struct sockaddr*)&c_in,&len);
 
-        if(r_len > 0)
-        {  
-            cout<<"image received!"<<endl;
-            /*Verify first package*/
-            if(buf[0]=='i' && buf[1]=='m' && buf[2]=='a' && buf[3]=='g' && buf[4]=='e')
-            {
-                int pkg_num = buf[5];
-                int last_pkg_bytes = buf[6]*256 + buf[7];
-                vector <uchar> rec_vec;
-
-                long int total_size = (pkg_num-1)*1024 + last_pkg_bytes;
-                //cout <<"received data length "<<total_size<<endl;
-                rec_vec.resize(total_size);
-
-                /*receive and reconstruct data*/
-                for(int i=0; i<pkg_num; i++)
+            if(r_len > 0)
+            {  
+                cout<<"image received!"<<endl;
+                /*Verify first package*/
+                if(image_buf[0]=='d' && image_buf[1]=='a' && image_buf[2]=='t' && image_buf[3]=='i')
                 {
-                    r_len = recvfrom(sock_clit,buf,1024,0,(struct sockaddr*)&c_in,&len);
-                    int pkg_size = 1024;
-                    if(i == pkg_num-1 && last_pkg_bytes!=0) pkg_size=last_pkg_bytes;
+                    int pkg_num = image_buf[5] + image_buf[4] * 256;
+                    int last_pkg_bytes = image_buf[6]*256 + image_buf[7];
+                    vector <uchar> rec_vec;
 
-                    //cout <<"pkg_size "<<pkg_size<<endl;
+                    long int total_size = (pkg_num-1)*1024 + last_pkg_bytes;
+                    //cout <<"received data length "<<total_size<<endl;
+                    rec_vec.resize(total_size);
 
-                    for(int j=0; j<pkg_size; j++)
-                        rec_vec[1024*i+j] = buf[j];
+                    /*receive and reconstruct data*/
+                    for(int i=0; i<pkg_num; i++)
+                    {
+                        r_len = recvfrom(sock_clit,image_buf,1024,0,(struct sockaddr*)&c_in,&len);
+                        int pkg_size = 1024;
+                        if(i == pkg_num-1 && last_pkg_bytes!=0) pkg_size=last_pkg_bytes;
+
+                        //cout <<"pkg_size "<<pkg_size<<endl;
+
+                        for(int j=0; j<pkg_size; j++)
+                            rec_vec[1024*i+j] = image_buf[j];
+
+                    }
+
+                    /*decode and show*/
+                    rec_img = imdecode(rec_vec,CV_LOAD_IMAGE_COLOR);
+                    if(!rec_img.empty())imshow("client", rec_img);
+                    quit_flag = waitKey(10);
+
+                    /* Find shiny points */
+                    cvtColor(rec_img, HSV_img, CV_BGR2HSV);
+                    vector<Mat> channels;
+                    split(HSV_img, channels);
+                    int nr= HSV_img.rows; // number of rows  
+                    int nc= HSV_img.cols; // total number of elements per line  
+
+                    Mat background(nr, nc, CV_8UC1, Scalar::all(0));
+                    for (int j=0; j<nr; j++) {  
+                      uchar* data_H= channels[0].ptr<uchar>(j); 
+                      uchar* data_S= channels[1].ptr<uchar>(j); 
+                      uchar* data_V= channels[2].ptr<uchar>(j);  
+                      uchar* data_Background = background.ptr<uchar>(j);
+
+                        for (int i=0; i<nc; i++) {  
+                            if(data_H[i] > 0 && data_H[i] < 256 && data_S[i] > 0 && data_S[i] < 256 && data_V[i] > 200 && data_V[i] < 256)
+                                data_Background[i] = 255;
+                        }                    
+                    }
+                    imshow("target", background);
+                    quit_flag = waitKey(10);
 
                 }
-
-                /*decode and show*/
-                rec_img = imdecode(rec_vec,CV_LOAD_IMAGE_COLOR);
-                if(!rec_img.empty())imshow("client", rec_img);
-                quit_flag = waitKey(10);
             }
-        }
-        //if(!input_frame.empty())imshow("client", input_frame); 
-        //quit_flag = waitKey(20);
 
+            trans_type = 2;
+        }
+        else if(trans_type == 2)
+        {
+            /*request cloud*/
+            char send_buff[8] = "";
+            send_buff[0] = 2;
+            send_buff[1] = 2;
+            send_buff[2] = 2;
+            send_data(send_buff);
+
+            broken_check = 0; //start count, if can not receive all data in 2s, break
+
+             /*recieve*/
+            int r_len=recvfrom(sock_clit,image_buf,1024,0,(struct sockaddr*)&c_in,&len);
+
+            if(r_len > 0)
+            {  
+                cout<<"cloud received!"<<endl;
+                /*Verify first package*/
+                if(image_buf[0]=='d' && image_buf[1]=='a' && image_buf[2]=='t' && image_buf[3]=='c')
+                {
+                    int pkg_num = image_buf[5] + image_buf[4] * 256;
+                    int last_pkg_bytes = image_buf[6]*256 + image_buf[7];
+                    vector <uchar> rec_vec;
+
+                    long int total_size = (pkg_num-1)*1024 + last_pkg_bytes;
+                    //cout <<"received data length "<<total_size<<endl;
+                    rec_vec.resize(total_size);
+
+                    /*receive and reconstruct data*/
+                    for(int i=0; i<pkg_num; i++)
+                    {
+                        r_len = recvfrom(sock_clit,image_buf,1024,0,(struct sockaddr*)&c_in,&len);
+                        int pkg_size = 1024;
+                        if(i == pkg_num-1 && last_pkg_bytes!=0) pkg_size=last_pkg_bytes;
+
+                        //cout <<"pkg_size "<<pkg_size<<endl;
+
+                        for(int j=0; j<pkg_size; j++)
+                            rec_vec[1024*i+j] = image_buf[j];
+
+                    }
+
+                    /*decode and show*/
+                    for(int i = 0; i < rec_vec.size() / 3; i++)
+                    {
+                        pcl::PointXYZ p;
+                        p.x = (double)rec_vec.at(3 * i) * RESOLUSTION - HALF_RANGE;
+                        //cout<<rec_vec.at(3 * i)<<", "<<p.x<<endl;
+                        p.y = (double)rec_vec.at(3 * i + 1) * RESOLUSTION - HALF_RANGE;
+                        p.z = (double)rec_vec.at(3 * i + 2) * RESOLUSTION - HALF_RANGE;
+                        //cout<<p.x<<","<<p.y<<","<<p.z<<endl;
+                        cloud.points.push_back(p);
+                    }
+                    sensor_msgs::PointCloud2 cloud2;
+                    pcl::toROSMsg(cloud, cloud2);
+                    cloud2.header.frame_id = "world";
+                    cloud_pub.publish(cloud2);
+                    cout << "cloud publish to rviz! \n" << endl;
+                    cloud.points.clear();
+                }
+            }
+
+            trans_type = 1;
+        }        
+
+        quit_flag = waitKey(100);
         ros::spinOnce();  
     } 
     
     destroyWindow("client");
+    destroyWindow("target");
     close(sock_clit);  
 
     return 0;
--- src/socket_sever.cpp
+++ src/socket_sever.cpp
@@ -11,9 +11,15 @@
 #include <sys/socket.h>  
 #include <arpa/inet.h>  
 #include <string.h>   
-  
+#include <pcl/point_types.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include "cv_bridge/cv_bridge.h"    
+#include "sensor_msgs/image_encodings.h" 
+
 #define PORT 10001  
 #define BYTE_SEND_INTERVAL 1
+#define HALF_RANGE 3.2
+#define RESOLUSTION 0.1
 
 using namespace std;
 using namespace cv;
@@ -22,10 +28,22 @@ struct sockaddr_in s_in;// address structure
 struct sockaddr_in c_in; 
 socklen_t in_len; 
 
-int send_data(unsigned char* data, long int size) //255k at most
+// global mat for image
+Mat rec_img;
+bool image_ready;
+
+// global mat for cloud
+pcl::PointCloud<pcl::PointXYZ> cloud;
+bool cloud_ready = false;
+int point_num = 0;
+int cur_num = 0;
+
+int send_data_char(unsigned char* data, long int size, unsigned char type) //65536k at most
 {
     /*package number and last package size*/
+    
     int pkg_num = size/1024; 
+    cout<<"pkg sent: "<<pkg_num<<endl;
     int last_pkg_bytes = size % 1024;
     if(last_pkg_bytes > 0) pkg_num += 1;
     else last_pkg_bytes = 1024;
@@ -33,14 +51,16 @@ int send_data(unsigned char* data, long int size) //255k at most
     unsigned char buffer[1024];
     for(int i=0; i<1024; i++)
         buffer[i] = 0;
+    unsigned char num_high = (unsigned char) (pkg_num / 256);
+    unsigned char num_low = (unsigned char) (pkg_num % 256);
 
     /*send mark*/
-    buffer[0] = 'i';
-    buffer[1] = 'm';
-    buffer[2] = 'a';
-    buffer[3] = 'g';
-    buffer[4] = 'e';
-    buffer[5] = (unsigned char) pkg_num; //255k at most
+    buffer[0] = 'd';
+    buffer[1] = 'a';
+    buffer[2] = 't';
+    buffer[3] = type;
+    buffer[4] = num_high;
+    buffer[5] = num_low; 
     buffer[6] = (unsigned char) (last_pkg_bytes / 256);
     buffer[7] = (unsigned char) (last_pkg_bytes % 256);
 
@@ -63,12 +83,115 @@ int send_data(unsigned char* data, long int size) //255k at most
     return 1;
 }
 
+int send_data_double(double *data, long int size) 
+{
+    /*package number and last package size*/
+    int pkg_num = size / 1024;  //1k bytes on time
+    int last_pkg_bytes = size % 1024;
+    if(last_pkg_bytes > 0)
+        pkg_num += 1;
+    else
+        last_pkg_bytes = 1024;
+
+    double buffer[1024];
+    for(int i = 0; i < 1024; i++) buffer[i] = 0;
+
+    /*send mark*/
+    buffer[0] = 2;
+    buffer[1] = 2;
+    buffer[2] = 2;
+    buffer[3] = 2;
+    buffer[4] = point_num;
+
+    buffer[5] = (double)pkg_num;  
+    buffer[6] = (double)(last_pkg_bytes / 256);
+    buffer[7] = (double)(last_pkg_bytes % 256);
+
+    sendto(sock_sev, buffer, 1024, 0, (struct sockaddr *)&c_in, sizeof(struct sockaddr));
+
+    /*send data*/
+    for(int j = 0; j < pkg_num; j++)
+    {
+        int pkg_size = 1024;
+        if(j == pkg_num - 1) pkg_size = last_pkg_bytes;
+
+        for(int i = 0; i < pkg_size; i++) buffer[i] = *(data + 1024 * j + i);
+
+        sendto(sock_sev, buffer, 1024, 0, (struct sockaddr *)&c_in, sizeof(struct sockaddr));
+    }
+
+    return 1;
+}
+
+void cloudCallback(const sensor_msgs::PointCloud2ConstPtr &msg)
+{
+    cout << "new cloud " << endl;
+    if(cloud_ready == true) return;
+
+    cloud.points.clear();
+    pcl::fromROSMsg(*msg, cloud);
+    cloud_ready = true;
+}
+
+
+void imageCallback(const sensor_msgs::ImageConstPtr& tem_msg)  
+{ 
+    cv_bridge::CvImagePtr cv_ptr;
+    //cout<<"tring"<<endl;
+    try    
+    {    
+        /*转化成CVImage*/    
+        cv_ptr = cv_bridge::toCvCopy(tem_msg, sensor_msgs::image_encodings::BGR8);    
+        cv::waitKey(1);   
+    }    
+    catch (cv_bridge::Exception& e)    
+    {    
+        //ROS_ERROR("Could not convert from '%s' to 'mono8'.", tem_msg->encoding.c_str());
+	cout<<"convert failed"<<endl;        
+	return;
+    }
+
+    rec_img = cv_ptr->image;
+    image_ready = true;
+    //cout<<"image received!"<<endl;
+}
+
+void timerCallback(const ros::TimerEvent &e)
+{
+    cout << "timer create cloud" << endl;
+    if(cloud_ready == true) return;
+
+    // create some cloud
+    cloud.points.clear();
+    int num = 0;
+    for(int i = -20; i <= 20; ++i)
+        for(int j = -20; j <= 20; ++j)
+            for(int k = -20; k <= 20; ++k)
+            {
+                pcl::PointXYZ p;
+                p.x = i * 0.1;
+                p.y = j * 0.1;
+                p.z = k * 0.1;
+                num++;
+                cloud.points.push_back(p);
+            }
+    // cout << "cloud received, mat:" << cloud_mat << endl;
+    cloud_ready = true;
+}
+
 
 /* @function main */
 int main( int argc, char **argv )
 {
     ros::init(argc, argv, "socket_server");
     ros::NodeHandle n; 
+
+    ros::Subscriber cloud_sub = n.subscribe("/ring_buffer/cloud2", 1, cloudCallback);
+    ros::Subscriber image_sub = n.subscribe("/zed/rgb/image_raw_color", 1, imageCallback);
+    ros::Duration(0.5).sleep();
+
+    // just for test. Create some cloud and publish
+    //ros::Timer timer = n.createTimer(ros::Duration(1.0), timerCallback);
   
     //socklen_t len;  
     unsigned char buf[1024]="";//content buff area 
@@ -84,66 +207,128 @@ int main( int argc, char **argv )
 
   	socklen_t len=sizeof(struct sockaddr);
 
-  	Size rec_size = Size(640,480);
-  	Mat rec_img	= Mat(rec_size, CV_8UC3);
-  	namedWindow("server"); 
+  	/*For image*/
+
+     //namedWindow("server"); 
 
     /*open camera*/
-    VideoCapture camera(-1); 
-    if(!camera.isOpened())  
-    {  
-        cout<<"can not open camera!"<<endl;
-        return -1;  
-    }
+    //VideoCapture camera(-1); 
+    // if(!camera.isOpened())  
+    // {  
+    //     cout<<"can not open camera!"<<endl;
+    //     return -1;  
+    // }
 
-    Mat input_frame;
+    //Mat input_frame;
     Mat resize_frame;
-    Size send_size = Size(1280,720); 
+    Size send_size = Size(640,480); 
 
      /*compress paras init*/
     vector<int> param= vector<int>(2); 
     param[0]=CV_IMWRITE_JPEG_QUALITY; 
-    param[1]=50;//default(95) 0-100 
+    param[1]=40;//default(95) 0-100 
 
-    vector<uchar> trans_buff;//buffer for coding  
+    vector<uchar> trans_buff_img;//buffer for coding  
+    vector<uchar> trans_buff_cloud;  // buffer for coding
 
     cout<<"begin"<<endl; 
 
     char quit_flag = ' '; 
- 
+    int wait_counter = 0;
+    int wait_limit = 10;
 
     while(ros::ok && quit_flag!='q'){ 
 
-        in_len = sizeof(c_in);  
+    	//cout<<"waiting"<<endl;        
+    	in_len = sizeof(c_in);  
         //清空接收缓存数组  
         memset(recvbuf, 0, sizeof(recvbuf));  
         //开始接收数据  
         int n = recvfrom(sock_sev, recvbuf, sizeof(recvbuf), 0, (struct sockaddr *)&c_in, &in_len);  
         cout<<(int)recvbuf[5]<<(int)recvbuf[6]<<(int)recvbuf[7]<<endl;
 
-        if(n > 0)
+        if(n > 0 && (int)recvbuf[5] == 1)  //if request for image
         {
             /*send*/
-            camera >> input_frame;
-            resize_frame = Mat(send_size, input_frame.type());
-            resize(input_frame, resize_frame, send_size, CV_INTER_CUBIC);
+            //camera >> input_frame;
+            //resize_frame = Mat(send_size, input_frame.type());
+            //resize(input_frame, resize_frame, send_size, CV_INTER_CUBIC);
+            // wait for cloud ready
+            
+            while(!image_ready && ros::ok() && wait_counter < wait_limit)
+            {
+                //cout << "wait for image" << endl;
+                wait_counter ++;
+                ros::spinOnce();
+                ros::Duration(0.05).sleep();
+            }
 
-            imencode(".jpg",resize_frame,trans_buff,param);
-            unsigned char* buff = &trans_buff[0];
+                wait_counter = 0;
+            cout<<"sending image"<<endl;
 
-            //send(sock, buff, trans_buff.size(), 0);
-            send_data(buff, trans_buff.size());
+            if(wait_counter < wait_limit)  //valid image recieved
+            {
+                resize_frame = Mat(send_size, rec_img.type());
+                resize(rec_img, resize_frame, send_size, CV_INTER_CUBIC);
 
-            cout<<"image size "<<trans_buff.size()<<endl;
+                imencode(".jpg",resize_frame,trans_buff_img,param);
+                unsigned char* buff = &trans_buff_img[0];
 
-            if(!input_frame.empty())imshow("server", resize_frame); 
+                send_data_char(buff, trans_buff_img.size(), 'i');
+
+                //cout<<"image size "<<trans_buff_img.size()<<endl;
+                image_ready = false;
+
+                    //if(!resize_frame.empty()) 
+    		//{
+    		//    imshow("server", resize_frame);
+    		 //   waitKey(1);		
+    		//}
+            }
+                else cout<<"No valid image!"<<endl;
         }
+
+        else if(n > 0 && (int)recvbuf[5] == 2) //if request for cloud
+        {
+            // wait for cloud ready
+            while(!cloud_ready && ros::ok() && wait_counter < wait_limit) 
+            {
+                cout << "wait for cloud" << endl;
+                wait_counter ++;
+                ros::spinOnce();
+                ros::Duration(0.05).sleep();
+            }
+
+            wait_counter = 0;
+            cout<<"sending cloud"<<endl;
+
+            if(wait_counter < wait_limit)  
+            {
+
+                // encode point cloud
+                trans_buff_cloud.clear();
+                for(int i = 0; i < cloud.points.size(); ++i)
+                {
+                    trans_buff_cloud.push_back((unsigned char)((cloud.points.at(i).x + HALF_RANGE)/0.1) );
+                    trans_buff_cloud.push_back((unsigned char)((cloud.points.at(i).y + HALF_RANGE)/0.1) );
+                    trans_buff_cloud.push_back((unsigned char)((cloud.points.at(i).z + HALF_RANGE)/0.1) );
+                    //cout<<"("<<cloud.points.at(i).x<<","<<cloud.points.at(i).y<<","<<cloud.points.at(i).z<<")"<<endl;
+		    //cout<<(unsigned char)((cloud.points.at(i).x + HALF_RANGE)/0.1)<<endl;
+                }
+
+                unsigned char *buff = &trans_buff_cloud[0];
+                send_data_char(buff, trans_buff_cloud.size(), 'c');
+            }
+	    cloud_ready = false;
+            
+        }
+
 
         quit_flag = waitKey(33);
        
         ros::spinOnce(); 
     }  
-    destroyWindow("server"); 
+    //destroyWindow("server"); 
     close(sock_sev);
 
     return 0;
